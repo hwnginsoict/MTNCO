dd088afb9d8e24761544399e61028f40436580b5
hwnginsoict <thoquyenv@gmail.com>
2024-07-29 09:56:11 +0700

    add

--- MTPOMO/POMO/test_n100_Solomon.py
+++ MTPOMO/POMO/test_n100_Solomon.py
@@ -52,7 +52,7 @@ tester_params = {
         'path': '../../Trained_models/100',  # directory path of pre-trained model and log files saved.
         'epoch': 10000,  # epoch version of pre-trained model to load.
     },
-    'test_episodes': 5000,
+    'test_episodes': 1000,
     'test_batch_size': 500,
     'augmentation_enable': True,
     'aug_factor': 8,
