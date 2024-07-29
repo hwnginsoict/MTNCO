0e12708f26ffa642ef45bd7ceaa79c0d70c2d7f5
hwnginsoict <thoquyenv@gmail.com>
2024-07-29 10:42:44 +0700

    add

---
+++ Baseline/VRPTW/POMO/test_n100_Solomon.py
@@ -0,0 +1,136 @@
+##########################################################################################
+# Machine Environment Config
+
+DEBUG_MODE = False
+USE_CUDA = not DEBUG_MODE
+CUDA_DEVICE_NUM = 0
+
+
+##########################################################################################
+# Path Config
+
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, "..")  # for problem_def
+sys.path.insert(0, "../..")  # for utils
+
+
+##########################################################################################
+# import
+
+import logging
+from utils.utils import create_logger, copy_all_src
+
+from VRPTester import VRPTester as Tester
+
+
+##########################################################################################
+# parameters
+
+env_params = {
+    'problem_type': 'VRPTW',
+    'problem_size': 100,
+    'pomo_size': 100,
+}
+
+model_params = {
+    'embedding_dim': 128,
+    'sqrt_embedding_dim': 128**(1/2),
+    'encoder_layer_num': 6,
+    'qkv_dim': 16,
+    'head_num': 8,
+    'logit_clipping': 10,
+    'ff_hidden_dim': 512,
+    'eval_type': 'argmax',
+}
+
+list = [
+    'c101', 'c102', 'c103', 'c104', 'c105', 'c106', 'c107', 'c108', 'c109',
+    'c201', 'c202', 'c203', 'c204', 'c205', 'c206', 'c207', 'c208',
+    'r101', 'r102', 'r103', 'r104', 'r105', 'r106', 'r107', 'r108', 'r109', 'r110', 'r111', 'r112',
+    'r201', 'r202', 'r203', 'r204', 'r205', 'r206', 'r207', 'r208', 'r209', 'r210', 'r211',
+    'rc101', 'rc102', 'rc103', 'rc104', 'rc105', 'rc106', 'rc107', 'rc108',
+    'rc201', 'rc202', 'rc203', 'rc204', 'rc205', 'rc206', 'rc207', 'rc208'
+]
+
+index = 0
+
+
+tester_params = {
+    'use_cuda': USE_CUDA,
+    'cuda_device_num': CUDA_DEVICE_NUM,
+    'model_load': {
+        'path': './result',  # directory path of pre-trained model and log files saved.
+        'epoch': 2000,  # epoch version of pre-trained model to laod.
+    },
+    'test_episodes': 64,
+    'test_batch_size': 64,
+    'augmentation_enable': True,
+    'aug_factor': 8,
+    'aug_batch_size': 8,
+    'test_data_load': {
+        'enable': True,
+        'filename': ''
+        # 'filename': '../../../Test_instances/Solomon100/data_VRPTW_c101.pt' 
+    },
+}
+if tester_params['augmentation_enable']:
+    tester_params['test_batch_size'] = tester_params['aug_batch_size']
+
+
+logger_params = {
+    'log_file': {
+        'desc': 'test_vrptw100',
+        'filename': 'log.txt'
+    }
+}
+
+
+##########################################################################################
+# main
+
+def main():
+    if DEBUG_MODE:
+        _set_debug_mode()
+
+    create_logger(**logger_params)
+    _print_config()
+
+    tester = Tester(env_params=env_params,
+                      model_params=model_params,
+                      tester_params=tester_params)
+
+    copy_all_src(tester.result_folder)
+
+    list_all = []
+
+    for i in range(len(list)):
+        tester_params['test_data_load']['filename'] = f'../../Test_instances/Solomon100/data_VRPTW_{list[index]}.pt'
+        result = tester.run()
+        list_all.append(result)
+        index += 1
+
+    print("All results:", list_all)
+
+    # tester._test_one_batch(1)
+
+
+def _set_debug_mode():
+    global tester_params
+    tester_params['test_episodes'] = 10
+
+
+def _print_config():
+    logger = logging.getLogger('root')
+    logger.info('DEBUG_MODE: {}'.format(DEBUG_MODE))
+    logger.info('USE_CUDA: {}, CUDA_DEVICE_NUM: {}'.format(USE_CUDA, CUDA_DEVICE_NUM))
+    [logger.info(g_key + "{}".format(globals()[g_key])) for g_key in globals().keys() if g_key.endswith('params')]
+
+
+
+##########################################################################################
+
+if __name__ == "__main__":
+    main()
