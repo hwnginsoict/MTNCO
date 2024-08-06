f37f4cf7764db2c19602e12d83b8397bae24bec7
hwnginsoict <thoquyenv@gmail.com>
2024-08-06 12:14:01 +0700

    add

--- MTPOMO/POMO/test_n100_Solomon.py
+++ MTPOMO/POMO/test_n100_Solomon.py
@@ -54,7 +54,7 @@ tester_params = {
         'path': '../../Trained_models/100',  # directory path of pre-trained model and log files saved.
         'epoch': 10000,  # epoch version of pre-trained model to load.
     },
-    'test_episodes': 1000,
+    'test_episodes': 5000,
     'test_batch_size': 500,
     'augmentation_enable': True,
     'aug_factor': 8,
@@ -89,7 +89,7 @@ def main():
     list_all = []
 
     for i in range(len(list)):
-        tester_params['test_data_load']['filename'] = f'../../Test_instances/Solomon100/data_VRPTW_{list[index]}.pt'
+        tester_params['test_data_load']['filename'] = f'../../Test_instances/test100/data_VRPTW_{list[index]}.pt'
         result = tester.run()
         list_all.append(result)
         index += 1
