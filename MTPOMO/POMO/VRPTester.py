999d4daddf5273c100e4e716de35c02306305dab
hwnginsoict <thoquyenv@gmail.com>
2024-08-12 12:37:46 +0700

    add

--- MTPOMO/POMO/VRPTester.py
+++ MTPOMO/POMO/VRPTester.py
@@ -157,7 +157,7 @@ class VRPTester:
         max_pomo_reward, index1 = aug_reward.max(dim=2)  # get best results from pomo
         # shape: (augmentation, batch)
 
-        index1 = int(index1[0][7])
+        index1 = int(index1)
         print(index1)
 
 
@@ -166,10 +166,10 @@ class VRPTester:
         max_aug_pomo_reward, index2 = max_pomo_reward.max(dim=0)  # get best results from augmentation
         # shape: (batch,)
 
-        index2 = int(index2[7])
+        index2 = int(index2)
         print(index2)
 
-        print(route[index1][index2])
+        # print(route[index1][index2])
 
         aug_score = -max_aug_pomo_reward.float().mean()  # negative sign to make positive value
 
