c73a4c3808ee0e16750948582f92bf21cf19ca2d
hwnginsoict <thoquyenv@gmail.com>
2025-01-21 12:47:30 +0700

    add

--- Baseline/VRPTW/POMO/test_n200_Solomon.py
+++ Baseline/VRPTW/POMO/test_n200_Solomon.py
@@ -1,26 +1,38 @@
-import os
-import sys
-import torch
-import logging
-
+##########################################################################################
 # Machine Environment Config
+
 DEBUG_MODE = False
 USE_CUDA = not DEBUG_MODE
 CUDA_DEVICE_NUM = 0
 
+
+##########################################################################################
 # Path Config
+
+import os
+import sys
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, "..")  # for problem_def
 sys.path.insert(0, "../..")  # for utils
 
+
+##########################################################################################
+# import
+
+import logging
 from utils.utils import create_logger, copy_all_src
+
 from VRPTester import VRPTester as Tester
 
-size = 100
 
-# Parameters
+##########################################################################################
+# parameters
+
+size = 20
+
 env_params = {
-    'problem_type': "VRPTW", # test problem type
+    'problem_type': 'VRPTW',
     'problem_size': size,
     'pomo_size': size,
 }
@@ -36,79 +48,93 @@ model_params = {
     'eval_type': 'argmax',
 }
 
+
 tester_params = {
     'use_cuda': USE_CUDA,
     'cuda_device_num': CUDA_DEVICE_NUM,
     'model_load': {
-        'path': '../../Trained_models/100',  # directory path of pre-trained model and log files saved.
-        'epoch': 10000,  # epoch version of pre-trained model to load.
+        'path': './result',  # directory path of pre-trained model and log files saved.
+        'epoch': 2000,  # epoch version of pre-trained model to laod.
     },
-    'test_episodes': 1000,
-    'test_batch_size': 100,
+    'test_episodes': 64,
+    'test_batch_size': 64,
     'augmentation_enable': True,
     'aug_factor': 8,
-    'aug_batch_size': 100,
+    'aug_batch_size': 8,
     'test_data_load': {
         'enable': True,
-        'filename': ''  # Placeholder, will be set in main()
+        'filename': ''
     },
 }
-
 if tester_params['augmentation_enable']:
     tester_params['test_batch_size'] = tester_params['aug_batch_size']
 
+
 logger_params = {
     'log_file': {
-        'desc': 'test_' + env_params['problem_type'] + '_n' + str(env_params['problem_size']) + '_with_instNorm',
-        'filename': 'run_log'
+        'desc': 'test_vrptw100',
+        'filename': 'log.txt'
     }
 }
 
+
+##########################################################################################
+# main
+
 def main():
-    global index  # Declare index as global
+    global index
     if DEBUG_MODE:
         _set_debug_mode()
 
     create_logger(**logger_params)
     _print_config()
 
-    import time
+    tester = Tester(env_params=env_params,
+                      model_params=model_params,
+                      tester_params=tester_params)
 
-    tester = Tester(env_params=env_params, model_params=model_params, tester_params=tester_params)
     copy_all_src(tester.result_folder)
 
     list_all = []
+
     route_all = []
-    time_list = []
+
+    time_list = []  
+
+    import time
 
     for i in range(1,4):
-
         start = time.time()
-        tester_params['test_data_load']['filename'] = f'/content/MTNCO/Test_instances/mock_data/data_VRPTW_{size}_{i}.pt'
+        tester_params['test_data_load']['filename'] = f'/content/MTNCO/Test_instances/Solomon100/data_VRPTW_{list[index]}.pt'
         result, route = tester.run()
         list_all.append(result)
         route_all.append(route)
-        
+
         run_time = time.time() - start
         time_list.append(run_time)
 
+
     print("All results:", list_all)
     print("All routes:", route_all)
-    print("Time:", run_time)
-
 
     print("Average results:", sum(list_all)/len(list_all))
     print("Average time:", sum(time_list)/len(time_list))
 
+
 def _set_debug_mode():
     global tester_params
     tester_params['test_episodes'] = 10
+
 
 def _print_config():
     logger = logging.getLogger('root')
     logger.info('DEBUG_MODE: {}'.format(DEBUG_MODE))
     logger.info('USE_CUDA: {}, CUDA_DEVICE_NUM: {}'.format(USE_CUDA, CUDA_DEVICE_NUM))
     [logger.info(g_key + "{}".format(globals()[g_key])) for g_key in globals().keys() if g_key.endswith('params')]
+
+
+
+##########################################################################################
 
 if __name__ == "__main__":
     main()
